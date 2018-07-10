eb3456d8c861ba04176e0794c68d1f409ae89137
Theo Zourzouvillys <theo@zrz.io>
2018-07-09 17:04:05 -0700

    add a basic grpc wrapper

---
+++ grpc/module-info.java
@@ -0,0 +1,20 @@
+module io.grpc {
+
+	exports io.grpc;
+	exports io.grpc.internal;
+	exports io.grpc.stub;
+	exports io.grpc.util;
+
+	requires java.logging;
+	requires java.naming;
+
+	// requires error.prone.annotations;
+	// requires guava;
+
+	uses io.grpc.ManagedChannelProvider;
+	uses io.grpc.NameResolverProvider;
+	uses io.grpc.ServerProvider;
+
+	provides io.grpc.NameResolverProvider with io.grpc.internal.DnsNameResolverProvider;
+
+}
